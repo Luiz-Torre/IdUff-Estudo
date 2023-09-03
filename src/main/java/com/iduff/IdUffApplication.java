8bcb7508dba7e466e3441d7e280869ff5c4e8c80
Luiz <luizmptorre@gmail.com>
2023-09-03 11:41:34 -0300

    Primeiro Commit

---
+++ src/main/java/com/iduff/IdUffApplication.java
@@ -0,0 +1,13 @@
+package com.iduff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdUffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdUffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iduff/IdUffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iduff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdUffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
